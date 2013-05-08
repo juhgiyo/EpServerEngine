7020726d5686a63c6446714813349a7a91d37cbe
Woong Gyu La <juhgiyo@gmail.com>
2013-05-08 11:28:02 +0900

    Engine Header updated

--- EpServerEngine2.0/EpServerEngine/Headers/epse.h
+++ EpServerEngine2.0/EpServerEngine/Headers/epse.h
@@ -30,33 +30,71 @@ An Interface for EP Server Engine
 #ifndef __EP_EPSE_H__
 #define __EP_EPSE_H__
 
-// Client Side
-#include "epBaseClient.h"
-#include "epBaseClientManual.h"
-#include "epBaseClientUDP.h"
-#include "epBaseClientUDPManual.h"
-
-// Server Side
-#include "epBaseServer.h"
-#include "epBaseServerWorker.h"
-
-// Server Worker
-#include "epBaseServerUDP.h"
-#include "epBaseServerWorkerUDP.h"
-
-// ServerObject List
+// General
+#include "epServerConf.h"
+#include "epPacket.h"
+#include "epBaseServerObject.h"
+#include "epPacketContainer.h"
+#include "epBasePacketProcessor.h"
 #include "epServerObjectList.h"
-#include "epParserList.h"
 #include "epServerObjectRemover.h"
 
-// General
-#include "epBasePacketParser.h"
-#include "epBaseServerObject.h"
-#include "epBaseServerSendObject.h"
-#include "epPacket.h"
-#include "epPacketContainer.h"
-#include "epServerConf.h"
 
+// Client Side
+#include "epClientInterfaces.h"
+
+#include "epBaseTcpClient.h"
+#include "epBaseUdpClient.h"
+#include "epBaseClient.h"
+
+#include "epClientPacketProcessor.h"
+#include "epAsyncTcpClient.h"
+#include "epAsyncUdpClient.h"
+
+#include "epSyncTcpClient.h"
+#include "epSyncUdpClient.h"
+
+#include "epIocpClientJob.h"
+#include "epIocpClientProcessor.h"
+#include "epIocpTcpClient.h"
+#include "epIocpUdpClient.h"
+
+
+// Server Side
+#include "epServerInterfaces.h"
+
+#include "epBaseSocket.h"
+#include "epBaseServer.h"
+#include "epBaseTcpSocket.h"
+#include "epBaseTcpServer.h"
+#include "epBaseUdpSocket.h"
+#include "epBaseUdpServer.h"
+
+#include "epServerPacketProcessor.h"
+#include "epAsyncTcpServer.h"
+#include "epAsyncTcpSocket.h"
+#include "epAsyncUdpServer.h"
+#include "epAsyncUdpSocket.h"
+
+#include "epSyncTcpServer.h"
+#include "epSyncTcpSocket.h"
+#include "epSyncUdpServer.h"
+#include "epSyncUdpSocket.h"
+
+#include "epIocpServerJob.h"
+#include "epIocpServerProcessor.h"
+#include "epIocpTcpServer.h"
+#include "epIocpTcpSocket.h"
+#include "epIocpUdpServer.h"
+#include "epIocpUdpSocket.h"
+
+#include "epProxyServerInterfaces.h"
+#include "epBaseProxyHandler.h"
+#include "epBaseProxyServer.h"
+#include "epProxyTcpHandler.h"
+#include "epProxyTcpServer.h"
+#include "epProxyUdpHandler.h"
+#include "epProxyUdpServer.h"
 
 
 #endif //__EP_EPSE_H__
